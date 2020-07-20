e0463aef906675ee5679b3b2f37d48485359648f
Robert Carter <racarter1215@gmail.com>
2020-07-20 12:16:34 -0700

    initial commit (Amanda provided base code)

---
+++ StacksandQueues/StacksandQueues/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace StacksAndQueues
+{
+    public class Node
+    {
+        public string Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(string value)
+        {
+            Value = value;
+        }
+    }
+}
