adddf658cf474845cdf27515fd968cbb4a6ca0ce
Robert Carter <racarter1215@gmail.com>
2020-07-27 22:04:39 -0700

    finished in and post order

--- tree/tree/Trees/Tree.cs
+++ tree/tree/Trees/Tree.cs
@@ -39,5 +39,48 @@ namespace tree.Trees
                 PreOrder(traversal, root.RightChild);
             }
         }
+
+        public List<T> InOrder(Node<T> root)
+        {
+            List<T> traversal = new List<T>();
+            InOrder(traversal, root);
+            return traversal;
+        }
+
+        private void InOrder(List<T> traversal, Node<T> root)
+        {            
+            if (root.LeftChild != null)
+            {
+                InOrder(traversal, root.LeftChild);
+            }
+
+            traversal.Add(root.Value);
+
+            if (root.RightChild != null)
+            {
+                InOrder(traversal, root.RightChild);
+            }
+        }
+        public List<T> PostOrder(Node<T> root)
+        {
+            List<T> traversal = new List<T>();
+            PostOrder(traversal, root);
+            return traversal;
+        }
+
+        private void PostOrder(List<T> traversal, Node<T> root)
+        {
+            if (root.LeftChild != null)
+            {
+                PostOrder(traversal, root.LeftChild);
+            }
+
+            if (root.RightChild != null)
+            {
+                PostOrder(traversal, root.RightChild);
+            }
+            traversal.Add(root.Value);
+        }
+
     }
 }
