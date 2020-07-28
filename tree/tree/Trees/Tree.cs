cc63d040744bbb4a56c4d2bf73c561740157b072
Robert Carter <racarter1215@gmail.com>
2020-07-27 21:52:52 -0700

    basic structures down

---
+++ tree/tree/Trees/Tree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tree.Trees
+{
+    public class Tree<T>
+    {
+        public Node<T> Root { get; set; }
+
+        public Tree()
+        {
+
+        }
+
+        public Tree(T value)
+        {
+            Node<T> root = new Node<T>(value);
+            Root = root;
+        }
+
+        public List<T> PreOrder(Node<T> root)
+        {
+            List<T> traversal = new List<T>();
+            PreOrder(traversal, root);
+            return traversal;
+        }
+
+        private void PreOrder(List<T> traversal, Node<T> root)
+        {
+            traversal.Add(root.Value);
+
+            if(root.LeftChild != null)
+            {
+                PreOrder(traversal, root.LeftChild);
+            }
+            if(root.RightChild != null)
+            {
+                PreOrder(traversal, root.RightChild);
+            }
+        }
+    }
+}
