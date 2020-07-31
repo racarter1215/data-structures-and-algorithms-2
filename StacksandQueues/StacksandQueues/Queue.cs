b6cd7e9876774fcb333a9b50e076c4a2677d92fa
Robert Carter <racarter1215@gmail.com>
2020-07-30 21:50:53 -0700

    finished dequeue

--- StacksandQueues/StacksandQueues/Queue.cs
+++ StacksandQueues/StacksandQueues/Queue.cs
@@ -36,5 +36,38 @@ namespace StacksAndQueues
             }
 
         }
+
+        /// <summary>
+        /// the below method takes a node out of queue
+        /// </summary>
+        /// <param name="value"></param>
+        public string Dequeue(string value)
+        {
+            // Create a new Node
+            Node node = new Node(value);
+
+            if (Front == null)
+            {
+                throw new Exception("The Queue is Empty");
+            }
+            string answer = Front.Value;
+            Front = Front.Next;
+            return answer;
+
+        }
+
+
+        /// <summary>
+        /// looks at the first node of a queue, if it is there
+        /// </summary>
+        /// <returns>the value of the first node</returns>
+        public string Peek()
+        {
+            if (Front == null)
+            {
+                throw new Exception("The Queue is Empty");
+            }
+            return Front.Value;
+        }
     }
 }
